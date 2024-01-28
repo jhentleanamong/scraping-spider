1ec28b90202d544424a5d100d1bbf7f3ff5bd8a6
Jhentle Anamong <jhentle.anamong@gmail.com>
2024-01-28 23:09:46 +0800

    Install filament/filament v3.2.16 package

--- config/app.php
+++ config/app.php
@@ -167,7 +167,8 @@ return [
             App\Providers\AuthServiceProvider::class,
             // App\Providers\BroadcastServiceProvider::class,
             App\Providers\EventServiceProvider::class,
-            App\Providers\RouteServiceProvider::class,
+            App\Providers\Filament\AppPanelProvider::class,
+        App\Providers\RouteServiceProvider::class,
         ])
         ->toArray(),
 
